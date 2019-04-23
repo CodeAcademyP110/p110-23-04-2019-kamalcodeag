216a6aea4eeeee9683cfaa64a935f2fa512ae58c
Kamal Guliyev <kamalag@code.edu.az>
2019-04-24 00:49:35 +0400

    home practice - there is a question related to the line 40

--- XMLHttpRequest/src/main.js
+++ XMLHttpRequest/src/main.js
@@ -1,38 +1,64 @@
-window.onload = function () {
+window.onload = function () 
+{
 
     const btnLoad = document.querySelector('#loadData');
 
     btnLoad.onclick = function()
     {
-        const http = new XMLHttpRequest();
+        // const http = new XMLHttpRequest();
 
-        http.onreadystatechange = function(e)
+        // http.onreadystatechange = function(e)
+        // {
+        //     if(http.readyState === 4 && http.status === 200)
+        //     {
+        //        const data = JSON.parse(http.responseText);
+
+        //        data.forEach(post => {
+        //            const col = document.createElement('div');
+        //            col.classList.add("col-md-4");
+
+        //            const h3 = document.createElement('h3');
+        //            h3.innerText = post.title.substring(0, 10);
+
+        //            const p = document.createElement('p');
+        //            p.innerText = post.body;
+
+        //            col.appendChild(h3);
+        //            col.appendChild(p);
+
+        //             posts.appendChild(col);
+        //        })
+        //     }
+        // }
+
+
+
+        // --- PRACTICE ---
+
+        let request = new XMLHttpRequest();
+
+        request.onreadystatechange = function(e) // ??? onload ???
         {
-            if(http.readyState === 4 && http.status === 200)
+            let received = JSON.parse(this.responseText);
+            // console.log(received);
+            // console.log(this.readyState);
+            // console.log(e);
+
+            received.forEach(rec =>
             {
-               const data = JSON.parse(http.responseText);
+                let col = document.querySelector(".col");
+                let info = document.createElement("p");
+                info.innerText = rec.title;
+                col.appendChild(info);
 
-               data.forEach(post => {
-                   const col = document.createElement('div');
-                   col.classList.add("col-md-4");
-
-                   const h3 = document.createElement('h3');
-                   h3.innerText = post.title.substring(0, 10);
-
-                   const p = document.createElement('p');
-                   p.innerText = post.body;
-
-                   col.appendChild(h3);
-                   col.appendChild(p);
-
-                    posts.appendChild(col);
-               })
-            }
+            })
         }
         
-        http.open("GET", "https://jsonplaceholder.typicode.com/posts");
-        http.send();
+        request.open("GET", "https://ghibliapi.herokuapp.com/films",true);
+        request.send();
     }
+
+        // --- PRACTICE ---
 }
 
 
